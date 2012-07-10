e423398238abb40d26ac3c8c8542c46370be2fd8
Federico Ramirez <fedra.arg@gmail.com>
2012-07-10 17:31:33 -0300

    Added all files

---
+++ Model/Context.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Reflection;
+using System.Collections.Concurrent;
+using System.Collections;
+using System.Collections.Generic;
+using Dapper;
+
+namespace Model
+{
+    /// <summary>
+    /// Extension class for Dapper with basic Insert, Update and Delete that works on OleDB
+    /// @Author: Federico Ramírez
+    /// @Copyright: Paradigma <http://paradigma.com.ar>
+    /// </summary>
+    public class Context : IDisposable
+    {
+        #region Properties
+        private bool TransactionActive = true;
+        private IDbConnection _conn;
+        public IDbConnection Connection
+        {
+            get
+            {
+                return _conn;
+            }
+        }
+        public ConnectionType Type { get; set; }
+        public enum ConnectionType
+        {
+            OleDB, SqlServer
+        }
+        public string ConnectionString { private set; get; }
+
+        private static readonly ConcurrentDictionary<RuntimeTypeHandle, string> TypeTableName = new ConcurrentDictionary<RuntimeTypeHandle, string>();
+        private IDbTransaction transaction;
+
+        public enum CascadeStyle { Collection, Single, All, None }
+        #endregion
+
+        public Context(string connectionString, ConnectionType type = ConnectionType.OleDB, bool useWebConfig = true)
+        {
+            this.ConnectionString = connectionString;
+            this.Type = type;
+
+            switch (type)
+            {
+                case ConnectionType.OleDB:
+                    _conn = new System.Data.OleDb.OleDbConnection(useWebConfig
+                        ? ReadConnectionString(connectionString)
+                        : connectionString);
+                    break;
+                default:
+                    _conn = new System.Data.SqlClient.SqlConnection(useWebConfig
+                        ? ReadConnectionString(connectionString)
+                        : connectionString);
+                    break;
+            }
+
+            _conn.Open();
+            transaction = _conn.BeginTransaction();
+        }
+
+        private string ReadConnectionString(string connectionString)
+        {
+            string conn = null;
+            try
+            {
+                conn = System.Configuration.ConfigurationManager.ConnectionStrings[connectionString].ConnectionString;
+            }
+            catch
+            {
+                throw new InvalidOperationException("Could not find connection string: " + connectionString);
+            }
+
+            if (string.IsNullOrEmpty(conn))
+            {
+                throw new InvalidOperationException("Could not find connection string: " + connectionString);
+            }
+
+            return conn;
+        }
+
+        public void Dispose()
+        {
+            if (_conn != null)
+            {
+                if (_conn.State == ConnectionState.Open)
+                {
+                    transaction.Dispose();
+                    _conn.Close();
+                }
+                _conn.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Updates an object on the database when the SubmitChanges method is called
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">The object to update</param>
+        /// <param name="cascade">Whether to update child/parent objects too</param>
+        public void UpdateOnSubmit<T>(T obj, CascadeStyle cascade = CascadeStyle.None) where T : class
+        {
+            if (obj == null) return;
+            if (!TransactionActive)
+            {
+                transaction = _conn.BeginTransaction();
+                TransactionActive = true;
+            }
+
+            Type type = typeof(T);
+            string tableName = GetTableName(type);
+
+            List<string> fields = new List<string>();
+            List<string> values = new List<string>();
+            List<object> data = new List<object>();
+
+            string pk = "id";
+            object pkValue = null;
+
+            foreach (PropertyInfo pi in type.GetProperties())
+            {
+                bool ispk = (from a in pi.GetCustomAttributes(false) where a.GetType().Name.Equals("KeyAttribute") select a).Any();
+                if (ispk || pi.Name.Equals(pk, StringComparison.OrdinalIgnoreCase))
+                {
+                    pk = pi.Name;
+                    pkValue = pi.GetValue(obj, null);
+                }
+                else
+                {
+                    if ((cascade == CascadeStyle.All || cascade == CascadeStyle.Collection)
+                        && IsCollection(pi.PropertyType) && pi.GetValue(obj, null) != null)
+                    {
+                        foreach (var child in pi.GetValue(obj, null) as ICollection)
+                        {
+                            MethodInfo mi = typeof(Context).GetMethod("UpdateOnSubmit");
+                            MethodInfo genericMethod = mi.MakeGenericMethod(new Type[] { child.GetType() });
+                            genericMethod.Invoke(this, new Object[] { child, cascade });
+                        }
+                    }
+                    else if ((cascade == CascadeStyle.All || cascade == CascadeStyle.Single)
+                        && !IsPrimitive(pi.PropertyType))
+                    {   // If it's not a primitive I'll try to insert it as a child object
+                        MethodInfo mi = typeof(Context).GetMethod("UpdateOnSubmit");
+                        MethodInfo genericMethod = mi.MakeGenericMethod(new Type[] { pi.PropertyType });
+                        genericMethod.Invoke(this, new Object[] { pi.GetValue(obj, null), cascade });
+                    }
+                    else
+                    {
+                        fields.Add("[" + pi.Name + "] = @" + pi.Name);
+                        values.Add("@" + pi.Name);
+                        data.Add(pi.GetValue(obj, null));
+                    }
+                }
+            }
+
+            if (pkValue == null)
+            {
+                throw new Exception("Could not find primary key");
+            }
+
+            IDbCommand com = _conn.CreateCommand();
+            com.Transaction = transaction;
+            com.CommandText = string.Format("UPDATE {0} SET {1} WHERE {2} = {3}", tableName, string.Join(", ", fields), pk, pkValue);
+            for (int i = 0; i < values.Count; i++)
+            {
+                IDbDataParameter p = com.CreateParameter();
+                p.ParameterName = values[i];
+                p.Value = data[i];
+                com.Parameters.Add(p);
+            }
+
+            try
+            {
+                com.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Deletes the object from the database when the method SubmitChanges is called
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="cascade">Whether the child/parent objects will also be deleted
+        /// Note: It's recommended to leave that kind of logic to the database engine</param>
+        public void DeleteOnSubmit<T>(T obj, CascadeStyle cascade = CascadeStyle.None) where T : class
+        {
+            if (!TransactionActive)
+            {
+                transaction = _conn.BeginTransaction();
+                TransactionActive = true;
+            }
+
+            Type type = typeof(T);
+            string tableName = GetTableName(type);
+
+            string pk = "id";
+            object pkValue = null;
+
+            foreach (PropertyInfo pi in type.GetProperties())
+            {
+                bool ispk = (from a in pi.GetCustomAttributes(false) where a.GetType().Name.Equals("KeyAttribute") select a).Any();
+                if (ispk || pi.Name.Equals(pk, StringComparison.OrdinalIgnoreCase))
+                {
+                    pk = pi.Name;
+                    pkValue = pi.GetValue(obj, null);
+                }
+                else if (cascade != CascadeStyle.None)
+                {
+                    if (pi.GetValue(obj, null) == null) continue;
+
+                    if ((cascade == CascadeStyle.Collection || cascade == CascadeStyle.All)
+                        && IsCollection(pi.PropertyType))
+                    {
+                        foreach (var child in pi.GetValue(obj, null) as ICollection)
+                        {
+                            MethodInfo mi = typeof(Context).GetMethod("DeleteOnSubmit");
+                            MethodInfo genericMethod = mi.MakeGenericMethod(new Type[] { child.GetType() });
+                            genericMethod.Invoke(this, new Object[] { child, cascade });
+                        }
+                    }
+                    else if ((cascade == CascadeStyle.Single || cascade == CascadeStyle.All)
+                        && !IsPrimitive(pi.PropertyType))
+                    {
+                        MethodInfo mi = typeof(Context).GetMethod("DeleteOnSubmit");
+                        MethodInfo genericMethod = mi.MakeGenericMethod(new Type[] { pi.PropertyType });
+                        genericMethod.Invoke(this, new Object[] { pi.GetValue(obj, null), cascade });
+                    }
+                }
+            }
+
+            IDbCommand com = _conn.CreateCommand();
+            com.Transaction = transaction;
+            com.CommandText = "DELETE FROM " + tableName + " WHERE [" + pk + "] = " + pkValue;
+            com.ExecuteNonQuery();
+        }
+
+        // TODO: Use MSIL or something better than default reflection...
+        /// <summary>
+        /// Adds an object to the insertion queue
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns>The id of the inserted object</returns>
+        public int InsertOnSubmit<T>(T obj) where T : class
+        {
+            if (obj == null) return 0;
+            if (!TransactionActive)
+            {
+                transaction = _conn.BeginTransaction();
+                TransactionActive = true;
+            }
+
+            Type type = typeof(T);
+            string tableName = GetTableName(type);
+
+            List<string> names = new List<string>();
+            List<string> values = new List<string>();
+            List<object> data = new List<object>();
+
+            string pk = "id";
+            PropertyInfo piPk = null;
+
+            foreach (PropertyInfo pi in type.GetProperties())
+            {
+                bool ispk = (from a in pi.GetCustomAttributes(false) where a.GetType().Name.Equals("KeyAttribute") select a).Any();
+                if (ispk || pi.Name.Equals(pk, StringComparison.OrdinalIgnoreCase))
+                {
+                    pk = pi.Name;
+                    piPk = pi;
+                }
+                else
+                {
+                    if (IsCollection(pi.PropertyType) && pi.GetValue(obj, null) != null)
+                    {
+                        foreach (var child in pi.GetValue(obj, null) as ICollection)
+                        {
+                            MethodInfo mi = typeof(Context).GetMethod("InsertOnSubmit");
+                            MethodInfo genericMethod = mi.MakeGenericMethod(new Type[] { child.GetType() });
+                            genericMethod.Invoke(this, new Object[] { child });
+                        }
+                    }
+                    else if (!IsPrimitive(pi.PropertyType))
+                    {   // If it's not a primitive I'll try to insert it as a child object
+                        MethodInfo mi = typeof(Context).GetMethod("InsertOnSubmit");
+                        MethodInfo genericMethod = mi.MakeGenericMethod(new Type[] { pi.PropertyType });
+                        int id = (int)genericMethod.Invoke(this, new Object[] { pi.GetValue(obj, null) });
+                        if (id > 0)
+                        {   // If the value was not null
+                            // Check the foreign key
+                            var fk = (from a in pi.GetCustomAttributes(false) where a.GetType().Name.Equals("ForeignKeyAttribute") select a).FirstOrDefault() as dynamic;
+                            string fkString = pi.Name + "Id";
+                            if (fk != null)
+                                fkString = fk.Name;
+
+                            names.Add("[" + fkString + "]");
+                            values.Add("@" + fkString);
+                            data.Add(id);
+                        }
+                    }
+                    else
+                    {
+                        names.Add("[" + pi.Name + "]");
+                        values.Add("@" + pi.Name);
+                        data.Add(pi.GetValue(obj, null));
+                    }
+                }
+            }
+
+            IDbCommand com = _conn.CreateCommand();
+            com.Transaction = transaction;
+            com.CommandText = string.Format("INSERT INTO {0} ({1}) VALUES ({2})", tableName, string.Join(", ", names), string.Join(", ", values));
+            for (int i = 0; i < values.Count; i++)
+            {
+                IDbDataParameter p = com.CreateParameter();
+                p.ParameterName = values[i];
+                p.Value = data[i];
+                com.Parameters.Add(p);
+            }
+
+            try
+            {
+                com.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
+
+            switch (this.Type)
+            {
+                case ConnectionType.OleDB:
+                    com.CommandText = string.Format("SELECT TOP 1 {0} FROM {1} ORDER BY {0} DESC", pk, tableName);
+                    break;
+                case ConnectionType.SqlServer:
+                    com.CommandText = string.Format("SELECT cast(SCOPE_IDENTITY() as int) AS '{0}'", pk);
+                    break;
+            }
+
+            int result = -1;
+            if (!int.TryParse(com.ExecuteScalar().ToString(), out result))
+            {
+                throw new Exception("Could not get last primary key");
+            }
+
+            piPk.SetValue(obj, result, null);
+            return result;
+        }
+
+        /// <summary>
+        /// Finds an object in the database
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="comparator"></param>
+        /// <returns></returns>
+        public IEnumerable<T> Find<T>(Func<T, bool> comparator) where T : class
+        {
+            Type type = typeof(T);
+            string tableName = GetTableName(type);
+            return this.Connection.Query<T>("SELECT * FROM [" + tableName + "]").Where(comparator);
+        }
+
+        public T Find<T>(string pkValue, string pkField = null) where T : class
+        {
+            Type type = typeof(T);
+            string tableName = GetTableName(type);
+
+            // get the primary key field
+            string pk;
+            if (!string.IsNullOrEmpty(pkField))
+            {
+                pk = pkField;
+            }
+            else
+            {
+                pk = "id";
+                foreach (PropertyInfo pi in type.GetProperties())
+                {
+                    bool ispk = (from a in pi.GetCustomAttributes(false) where a.GetType().Name.Equals("KeyAttribute") select a).Any();
+                    if (ispk || pi.Name.Equals(pk, StringComparison.OrdinalIgnoreCase))
+                    {
+                        pk = pi.Name;
+                        break;
+                    }
+                }
+            }
+
+            return this.Connection.Query<T>("SELECT * FROM [" + tableName + "] WHERE [" + pk + "] = '" + pkValue + "'").FirstOrDefault();
+        }
+
+        public bool SubmitChanges()
+        {
+            try
+            {
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
+
+            TransactionActive = false;
+            transaction.Dispose();
+
+            return true;
+        }
+
+        private bool IsCollection(System.Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(ICollection<>);
+        }
+
+        private bool IsPrimitive(System.Type type)
+        {
+            return type.IsPrimitive || type == typeof(Char) || type == typeof(String) || type == typeof(Decimal) || type == typeof(DateTime);
+        }
+
+        private string GetTableName(Type type)
+        {
+            string name;
+            if (!TypeTableName.TryGetValue(type.TypeHandle, out name))
+            {
+                name = type.Name + "s";
+                if (type.IsInterface && name.StartsWith("I"))
+                    name = name.Substring(1);
+
+                //NOTE: This as dynamic trick should be able to handle both our own Table-attribute as well as the one in EntityFramework 
+                var tableattr = type.GetCustomAttributes(false).Where(attr => attr.GetType().Name == "TableAttribute").SingleOrDefault() as
+                    dynamic;
+                if (tableattr != null)
+                    name = tableattr.Name;
+                TypeTableName[type.TypeHandle] = name;
+            }
+            return name;
+        }
+    }
+
+    #region Attributes for classes and properties
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TableAttribute : Attribute
+    {
+        public TableAttribute(string tableName)
+        {
+            Name = tableName;
+        }
+        public string Name { get; private set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class KeyAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ForeignKeyAttribute : Attribute
+    {
+        public ForeignKeyAttribute(string fkName)
+        {
+            Name = fkName;
+        }
+        public string Name { get; private set; }
+    }
+    #endregion
+}
---
+++ Tests/TestContext.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dapper;
+using Model;
+
+namespace Tests
+{
+    /// <summary>
+    /// Test the model's context class
+    /// </summary>
+    [TestClass]
+    public class TestContext
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Assert.AreNotEqual(db, null);
+            }
+        }
+
+        [TestMethod]
+        public void TestBasicCRUD()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Note n = new Note();
+                n.Content = "Test content";
+                n.Created = DateTime.UtcNow.Date;
+                n.Modified = DateTime.UtcNow.Date;
+
+                db.InsertOnSubmit<Note>(n);
+                db.SubmitChanges();
+
+                Assert.AreNotEqual(n.Codigo, 0);
+
+                Note result = db.Connection.Query<Note>("SELECT * FROM Notes WHERE Codigo = @Id", new { Id = n.Codigo }).FirstOrDefault();
+                Assert.AreNotEqual(result, null);
+                Assert.AreEqual(result.Content, n.Content);
+                Assert.AreEqual(result.Created, n.Created);
+                Assert.AreEqual(result.Modified, n.Modified);
+
+                db.DeleteOnSubmit<Note>(result);
+                db.SubmitChanges();
+
+                Assert.IsFalse(db.Connection.Query<Post>("SELECT * FROM Notes WHERE Codigo = @Id", new { Id = n.Codigo }).Any());
+            }
+        }
+
+        [TestMethod]
+        public void TestChainedInsertUsingCollection()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Post p = new Post();
+                p.Content = "Test content 'n' escapes!";
+
+                Author a = new Author();
+                a.Username = "Mike";
+                a.Posts = new List<Post>();
+                a.Posts.Add(p);
+
+                int id = db.InsertOnSubmit<Author>(a);
+
+                Assert.AreEqual(a.Id, id);
+                Assert.AreEqual(a.Posts.First(), p);
+
+                db.DeleteOnSubmit<Author>(a, Context.CascadeStyle.Collection); // force cascading delete
+
+                db.SubmitChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestChainedInsertSimple()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Author a = new Author();
+                a.Username = "John";
+
+                Post p = new Post();
+                p.Content = "Test content";
+                p.Author = a;
+
+                int id = db.InsertOnSubmit<Post>(p);
+
+                Assert.AreEqual(id, p.Id);
+                Assert.AreEqual(p.Author, a);
+                Assert.AreNotEqual(p.Author.Id, 0);
+
+                db.DeleteOnSubmit<Post>(p, Context.CascadeStyle.Single); // force cascading delete
+
+                db.SubmitChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdateSimple()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Note n = new Note();
+                n.Content = "Prueba";
+                n.Created = DateTime.UtcNow.Date;
+                n.Modified = DateTime.UtcNow.Date;
+
+                db.InsertOnSubmit<Note>(n);
+                db.SubmitChanges();
+
+                n.Content = "Test";
+                db.UpdateOnSubmit<Note>(n);
+                db.SubmitChanges();
+
+                Note newNote = db.Connection.Query<Note>("SELECT * FROM Notes WHERE Codigo = " + n.Codigo).FirstOrDefault();
+
+                Assert.AreEqual(newNote.Content, "Test");
+
+                db.DeleteOnSubmit<Note>(newNote);
+                db.SubmitChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdateCollection()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Author a = new Author();
+                a.Username = "Jenny";
+                a.Posts = new List<Post>();
+
+                Post p = new Post();
+                p.Content = "Test post content";
+
+                Post pp = new Post();
+                pp.Content = "Another test content";
+
+                a.Posts.Add(p);
+                a.Posts.Add(pp);
+
+                db.InsertOnSubmit<Author>(a); // assume it works, tested on other methods
+                db.SubmitChanges();
+
+                a.Posts.First().Content = "Last content";
+                db.UpdateOnSubmit<Author>(a, Context.CascadeStyle.All);
+                db.SubmitChanges();
+
+                Post compare = db.Connection.Query<Post>("SELECT * FROM Posts WHERE Id = " + a.Posts.First().Id).FirstOrDefault();
+
+                Assert.AreEqual(compare.Content, a.Posts.First().Content);
+
+                db.DeleteOnSubmit<Author>(a, Context.CascadeStyle.All);
+                db.SubmitChanges();
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdateSingle()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Author a = new Author();
+                a.Username = "John";
+
+                // Now let's use the inserted author
+                Post p = new Post();
+                p.Content = "Test";
+                p.Author = a;
+
+                db.InsertOnSubmit<Post>(p);
+                db.SubmitChanges();
+
+                //db.UpdateOnSubmit<Post>(test);
+                //db.SubmitChanges();
+
+                //Author compare = db.Connection.Query<Author>("SELECT * FROM Authors WHERE Id = " + test.Author.Id).FirstOrDefault();
+                //Assert.AreEqual(compare, test.Author);
+            }
+        }
+
+        [TestMethod]
+        public void TestFind()
+        {
+            using (Model.Context db = new Model.Context("mdbConnectionString"))
+            {
+                Post p = new Post();
+                p.Content = "Dummy post";
+
+                db.InsertOnSubmit<Post>(p);
+                db.SubmitChanges();
+
+                Post found = db.Find<Post>(x => { return x.Content == "Dummy post"; }).FirstOrDefault();
+
+                Assert.AreNotEqual(found, null);
+                Assert.AreEqual(found.Content, p.Content);
+                Assert.IsTrue(found.Id > 0);
+            }
+        }
+    }
+
+    #region POCOs
+
+    class Note
+    {
+        [Key]
+        public int Codigo { get; set; }
+        public string Content { get; set; }
+        public DateTime Modified { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+    class Post
+    {
+        public int Id { get; set; }
+        //[ForeignKey("AuthorId")] // optional foreign key specification, AuthorId is the default value
+        public Author Author { get; set; }
+        public string Content { get; set; }
+    }
+
+    class Author
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+
+    #endregion
+}
